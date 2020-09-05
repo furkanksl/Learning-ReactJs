421a89e0b0a133b02dd0dfd94ea987ddefde084d
furkanksl <furkankoseoglu65@gmail.com>
2020-09-05 14:15:13 +0300

    use of setState in ReactJs

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,10 +8,26 @@ class Message extends Component {
             message : 'Welcome visitor'
         }
     }
+    changeMessage(){
+        
+        if(this.state.message === "Subscribed"){
+            this.setState({
+                message : "Welcome visitor"
+            })
+        }else if(this.state.message === "Welcome visitor"){
+            this.setState({
+                message : "Subscribed"
+            })
+        }
+    }
 
     render(){
         return (
-            <h1>{this.state.message}</h1>
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={()=> this.changeMessage()} >Subscribe</button>
+            </div>
+            
             )
     }
 }
