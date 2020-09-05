9dbc03c6fe68fbca018db9842a4c7e3f41937c3c
furkanksl <furkankoseoglu65@gmail.com>
2020-09-05 15:23:04 +0300

    setState sample

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { Greet } from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Counter />
+
+      {/*
       <Message />
-      {/*<Greet name="furkan" heroName="Batman" > 
+      <Greet name="furkan" heroName="Batman" > 
       <p>This is children props 1</p>
       </Greet>
       <Greet name="fatih" heroName="Deadpol" >
