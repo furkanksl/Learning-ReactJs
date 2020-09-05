9a2f0d43e7e3378abe184ee323e1caa34a8a5a40
furkanksl <furkankoseoglu65@gmail.com>
2020-09-05 14:08:43 +0300

    use of state in ReactJs

--- src/App.js
+++ src/App.js
@@ -4,24 +4,22 @@ import './App.css';
 import { Greet } from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="furkan" heroName="Batman" > 
+      <Message />
+      {/*<Greet name="furkan" heroName="Batman" > 
       <p>This is children props 1</p>
       </Greet>
       <Greet name="fatih" heroName="Deadpol" >
         <p>This is children props 2 </p>
       </Greet>
-     {/*  <Hello/>
+       <Hello/>
       <Welcome/> */}
     </div>
   );
-}
-
-function sum(a,b){
-  return a + b;
 }
 
 export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// it is a Class Component
+class Message extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            message : 'Welcome visitor'
+        }
+    }
+
+    render(){
+        return (
+            <h1>{this.state.message}</h1>
+            )
+    }
+}
+
+export default Message;
